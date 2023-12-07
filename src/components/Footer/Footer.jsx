6c4cf205d95265b1c41bb7403712de59bb73519b
Nitin-Magdum <nitinmagdum111@gmail.com>
2023-12-07 16:59:30 +0530

    added the new button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { footerVariants, staggerChildren } from "../../utils/motion";
 import css from "./Footer.module.scss";
 import {motion} from 'framer-motion'
+
 const Footer = () => {
   return (
     <motion.section
@@ -27,6 +28,7 @@ const Footer = () => {
 
         <div className={css.right}>
           <div className={css.info}>
+       
             <a className="secondaryText" href="https://www.linkedin.com/in/dktenitinmagdum" target="_blank">LinkedIn</a>
             <a className="secondaryText" href="https://github.com/Nitin-Magdum" target="_blank">GitHub</a>
             <a className="secondaryText" href="https://www.instagram.com/nitinmagdum/" target="_blank">Instagram</a>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import { fadeIn, slideIn, staggerContainer } from "../../utils/motion";
 import css from "./Hero.module.scss";
 import { motion } from "framer-motion";
-import pdfFile  from "../../Resume/NitinMagdum_Resume.docx.pdf"
+import pdfFile  from "../../Resume/NitinMagdum_Resume.pdf"
 import ReactTyped from "react-typed";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import DescriptionIcon from '@material-ui/icons/Description';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 const Hero = () => {
+  const classes = useStyles();
    const handleDownloadPdf = () => {
     // Create an anchor element
     const anchor = document.createElement("a");
     anchor.href = pdfFile; // Set the href to the imported PDF file
-    anchor.download = "NitinMagdum_Resume.docx.pdf"; // Set the file name for download
+    anchor.download = "NitinMagdum_Resume.pdf"; // Set the file name for download
     anchor.click();
   };
 
@@ -55,7 +65,17 @@ const Hero = () => {
           </motion.div>
 
           <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className={css.certificate}>
-            <button className={css.button}onClick={handleDownloadPdf}>Resume</button>
+            <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            className={classes.button}
+            startIcon={<DescriptionIcon />}
+            onClick={handleDownloadPdf}
+          >
+            Resume
+          </Button>
+
               <br/>
           </motion.div>
         </div>
